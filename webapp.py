04c887c6962044dfb5e999616a6427cc2e0408f9
Vsimpro <jsimrpo@gmail.com>
2021-07-20 21:20:13 +0000

    Fixing tony.

--- webapp.py
+++ webapp.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="/root/pojat-kosi")
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/index.html")
+def return_homepage():
+    return redirect("/")
+
 if __name__ == "__main__":
-    app.run(host="")
+    app.run(host="0.0.0.0")
