350ebe2a5808d7b949b4c56e79bc5a9d638b9b26
Vsimpro <jsimrpo@gmail.com>
2021-07-22 10:15:38 +0000

    agile development

--- webapp.py
+++ webapp.py
@@ -10,13 +10,21 @@ def index():
 def return_homepage():
     return redirect("/")
 
-@app.route("/tony")
+@app.route("/tony.html")
 def tony():
+    return redirect("/tony")
+
+@app.route("/tony")
+def return_tony():
     return render_template("tony.html")
 
 @app.route("/alibi")
 def alibi():
     return render_template("alibi.html")
+
+@app.route("/alibi.html")
+def return_alibi():
+    return redirect("/alibi")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
