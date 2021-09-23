4052b91ccee3051c164722393f0c4feb03941ea7
Vsim <73603125+Vsimpro@users.noreply.github.com>
2021-09-23 19:51:24 +0300

    added "kärähdys" into "alibi"

--- webapp.py
+++ webapp.py
@@ -10,5 +10,15 @@ def index():
 def hello_world(file):
     return render_template(file)
 
+@app.route("/alibi.html")
+def alibi():
+    datal = list()
+    with open("alibi.txt", "r") as file:
+        for i in file.readlines():
+            datal.append(i)
+            print(i)
+
+    return render_template("/alibi.html",data=datal)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
